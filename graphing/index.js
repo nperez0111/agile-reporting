9679a5634b324c8135576c7c694ac4bca285c35f
STEVEN ROKKALA <38511253+srokkala@users.noreply.github.com>
2020-05-03 21:12:53 -0700

    Changes

--- graphing/index.js
+++ graphing/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const data  = require('./dataGen');
 
 app.use('/data',(req,res)=>{
   /**
@@ -7,13 +8,22 @@ app.use('/data',(req,res)=>{
    * 
    * Will eventually use `/io` to get the data but for now it is okay to just use an array here directly
    */
+  // Read Data.json file and send it to front end
 
-  return res.json([{value:0.45, label: (new Date()).toString()}])
+  // data().then(datareturned =>{
+  //   for(let i = 0; i < datareturned.length; i++){
+  //     console.log(datareturned[i].value);
+  //   }
+  // });
+
+console.log(res.json());
+
+  return data();
 });
 
 app.use(express.static(__dirname + '/html'));
 
 app.listen(3000, () => {
-  console.log('app listening on port 3000!')
+  console.log('app listening on port 3000!');
   console.log('go to: http://localhost:3000/ in your browser')
 });
